2f787c4e49c3cac36250e15bb56935342964ce28
SamuellRodrigues <baurukigamer@gmail.com>
2024-09-18 15:23:34 -0300

    Pedra,Papel,Tesousa

---
+++ PedraPapelTesoura/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        string[] options = { "Pedra", "Papel", "Tesoura" };
+
+        Console.WriteLine("Escolha: Pedra, Papel ou Tesoura (ou 'sair' para encerrar):");
+
+        while (true)
+        {
+            string userChoice = Console.ReadLine();
+
+            if (userChoice.ToLower() == "sair")
+                break;
+
+            
+            string computerChoice = options[random.Next(options.Length)];
+            Console.WriteLine($"Computador escolheu: {computerChoice}");
+
+            if (userChoice == computerChoice)
+            {
+                Console.WriteLine("Empate!");
+            }
+            else if ((userChoice == "Pedra" && computerChoice == "Tesoura") ||
+                     (userChoice == "Tesoura" && computerChoice == "Papel") ||
+                     (userChoice == "Papel" && computerChoice == "Pedra"))
+            {
+                Console.WriteLine("Você venceu!");
+            }
+            else
+            {
+                Console.WriteLine("Computador venceu!");
+            }
+
+            
+            Console.WriteLine("Escolha: Pedra, Papel ou Tesoura (ou 'sair' para encerrar):");
+        }
+    }
+}
